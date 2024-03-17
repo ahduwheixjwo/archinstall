19f281c0b0f1100295abcdc704d923f421769cf7
ahduwheixjwo <rmnaml2468@gmail.com>
2024-03-17 16:52:11 +0800

    Added configuration script

---
+++ Scripts/configure.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+set -e
+
+# Set time zone
+ln -s /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+hwclock --systohc
+
+# Set localization
+local="en_US.UTF-8"
+
+# Check if the locale existed and uncommented
+if grep -q "^#$local" /etc/locale.gen; then
+    # Uncomment the locale
+    sed -i "s/^#$local/$local/" /etc/locale.gen
+fi
+
+# Update locale
+locale-gen
+echo LANG=$local > /etc/locale.conf
+
+# Hostname
+while true; do
+    echo "Enter your preferred hostname"
+    read -p ">>" hostname
+    echo ""
+
+    if [[ -z "$hostname" ]];then
+        echo "Please enter your preferred hostname..."
+        echo ""
+    else
+        break
+    fi
+done
