48e3ad18554fb2526512a13e1176b5f3e7c6283a
ahduwheixjwo <rmnaml2468@gmail.com>
2024-03-20 13:53:42 +0800

    Refactor installation script to encapsulate configuration steps

--- Scripts/configure.sh
+++ Scripts/configure.sh
@@ -2,41 +2,43 @@
 
 set -e
 
-# Set time zone
-ln -sf /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
-hwclock --systohc
+configure() {
+    # Set time zone
+    ln -sf /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
+    hwclock --systohc
 
-# Set localization
-local="en_US.UTF-8"
+    # Set localization
+    local="en_US.UTF-8"
 
-# Check if the locale existed and uncommented
-if grep -q "^#$local" /etc/locale.gen; then
-    # Uncomment the locale
-    sed -i "s/^#$local/$local/" /etc/locale.gen
-fi
-
-# Update locale
-locale-gen
-echo LANG=$local > /etc/locale.conf
-
-# Set system hostname
-while true; do
-    echo "Enter your preferred hostname"
-    read -p ">>" hostname
-    echo ""
-
-    if [[ -z "$hostname" ]];then
-        echo "Please enter your preferred hostname..."
-        echo ""
-    else
-        break
+    # Check if the locale existed and uncommented
+    if grep -q "^#$local" /etc/locale.gen; then
+        # Uncomment the locale
+        sed -i "s/^#$local/$local/" /etc/locale.gen
     fi
-done
-echo "$hostname" > /etc/hostname
 
-# Configuring hostname resolution
-cat << EOF | tee -a /etc/hosts >/dev/null 2>&1
+    # Update locale
+    locale-gen
+    echo LANG=$local > /etc/locale.conf
+
+    # Set system hostname
+    while true; do
+        echo "Enter your preferred hostname"
+        read -p ">>" hostname
+        echo ""
+
+        if [[ -z "$hostname" ]];then
+            echo "Please enter your preferred hostname..."
+            echo ""
+        else
+            break
+        fi
+    done
+    echo "$hostname" > /etc/hostname
+
+    # Configuring hostname resolution
+    cat << EOF | tee -a /etc/hosts >/dev/null 2>&1
 127.0.0.1   localhost
 ::1         localhost
-127.0.1.1   "$hostname".localdomain "$hostname"
+127.0.1.1   $hostname.localdomain $hostname
 EOF
+}
